0e47e96e8494c5a8dac553e611c0a22a8cf4ce04
msk6252 <msk6252@yahoo.co.jp>
2021-09-23 11:11:53 +0900

    first commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Header />
+    <OpenDataMap />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import OpenDataMap from './components/OpenDataMap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer,
+    OpenDataMap
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/components/OpenDataMap.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+import axios from 'axios'
+
+delete L.Icon.Default.prototype._getIconUrl
+
+L.Icon.Default.mergeOptions(
+  {
+    iconUrl : require('leaflet/dist/images/marker-icon.png'),
+    iconRetinaUrl   : require( 'leaflet/dist/images/marker-icon-2x.png' ),
+    shadowUrl       : require( 'leaflet/dist/images/marker-shadow.png' )
+  }
+)
+
+export default {
+  name: 'OpenDataMap',
+  methods: {
+    getMarker: () => {
+      // axios.get(path, request).then(response => {}).catch(error => {console.log(error)})
+      return [[39.19767, 141.188945],[39.195669, 141.188779],[39.154929, 141.359271]]
+    }
+  },
+  mounted() {
+    let map = L.map( 'app', { center: L.latLng( 39.14481501086482, 141.13926409203623 ), zoom: 12 } ).addLayer(
+      L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png')
+    )
+    this.getMarker().forEach(ele => {
+      let marker = map.addLayer( L.marker(ele) )
+      map.on('click', (e) => {
+        let popup = L.popup()
+        popup
+          .setLatLng(e.latlng)
+          .setContent("<p>クリックで表示される</p>")
+          .openOn(map)
+      })
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+html, body, #app { height: 100% }
+body: { margin: 0 }
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
